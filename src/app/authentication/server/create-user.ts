26d09c9073eb27f1649e8fdc13a955b9917d88d8
noah <91755574+noahSnbr07@users.noreply.github.com>
2025-03-18 10:51:50 +0100

    removed action rsponse

--- src/app/authentication/server/create-user.ts
+++ src/app/authentication/server/create-user.ts
@@ -5,20 +5,11 @@ import ActionRespose from "@/app/types/ActionResponse";
 /**
  * @param formData: FormData
  */
-export default async function createUser(formData: FormData): Promise<ActionResponse> | null {
+export default async function createUser(formData: FormData) {
 
-    const email = formData.get("email");
-    const password = formData.get("password");
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
 
-    // Ensure both email and password are strings
-    if (typeof email !== 'string' || typeof password !== 'string') {
-        return {
-            success: false,
-            status: 400,
-            title: "Invalid Form Data",
-            description: "Email and password must be strings.",
-        } as ActionRespose;
-    }
 
     console.log({ email, password });
 
@@ -26,40 +17,6 @@ export default async function createUser(formData: FormData): Promise<ActionResp
     const missingPassword = !password || password.length < 1;
     const missingEmail = !email || email.length < 1;
 
-    if (missingEmail || missingPassword) {
-        return {
-            success: false,
-            status: 400,
-            title: "Invalid Form Data",
-            description: "Both email and password are required.",
-        } as ActionRespose;
-    }
+    console.log({ missingEmail, missingPassword })
 
-    try {
-        console.log("User data is valid, proceeding with creation...");
-
-        return {
-            success: true,
-            status: 200,
-            title: "Successful Operation",
-            description: "User created successfully.",
-        };
-
-    } catch (error: unknown) {
-        if (error instanceof Error) {
-            return {
-                success: false,
-                status: 500,
-                title: "Internal Server Error",
-                description: error.message,
-            };
-        }
-
-        return {
-            success: false,
-            status: 500,
-            title: "Unknown Server Error",
-            description: "An unknown error occurred.",
-        };
-    }
 }
